067372fd1f5f59b2fc56cb14b68ebede9e4d82b3
Alex F <93062390+alexfrnn@users.noreply.github.com>
2024-11-16 21:28:33 +0000

    chore: add a emoji on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alex Fernando",
+  name: "Alex Fernando 🧗‍♂️",
   title: "Software developer & data analyst",
   email: "",
   gitHub: "alexfrnn",
