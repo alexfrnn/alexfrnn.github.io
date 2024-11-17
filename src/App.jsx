461504a796edff38fc4a2cf7cc3d08d10523d404
Alex F <93062390+alexfrnn@users.noreply.github.com>
2024-11-17 01:34:37 +0000

    chore: update name in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alex Fernando 🧗‍♂️",
+  name: "Alex Fernando ",
   title: "Software developer & data analyst",
   email: "",
   gitHub: "alexfrnn",
